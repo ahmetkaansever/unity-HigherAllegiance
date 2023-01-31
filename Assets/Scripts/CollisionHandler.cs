5ae8fd3da07e875d5e45bdd1c2a68fb17e9d9687
Ahmet Kaan Sever <ahmetsever2003@yahoo.com>
2023-02-01 00:30:06 +0300

    Added destruction for player and enemy planes

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other)
-    {
-        Debug.Log($"{this.name} collided with {other.gameObject.name}");
-    }
-
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem crashVFX;
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{this.name} triggered by {other.name}");
+        StartCrashSequence();
+    }
+
+    void StartCrashSequence()
+    {
+        crashVFX.Play();
+        GameObject visualParts = this.transform.Find("Parts").gameObject;
+        for(int i = 0; i < visualParts.transform.childCount; i++)
+        {
+            visualParts.transform.GetChild(i).GetComponent<MeshRenderer>().enabled = false;
+        }
+        visualParts.transform.Find("Spitfire_body").GetComponent<MeshCollider>().enabled = false;
+        GetComponent<PlayerControls>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+   private void OnParticleCollision(GameObject other) 
+   {    
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        Destroy(gameObject);
+   }
+}
