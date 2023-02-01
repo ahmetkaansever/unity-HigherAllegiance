80ded258b6f8f4ef52af7077e6e49cd4950431c3
Ahmet Kaan Sever <ahmetsever2003@yahoo.com>
2023-02-01 20:37:21 +0300

    Added score system

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,10 +6,20 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int enemyWorth = 15;
+    ScoreBoard scoreBoard;
+    
+
+    void Start()
+    {
+          scoreBoard = FindObjectOfType<ScoreBoard>();     
+    }
    private void OnParticleCollision(GameObject other) 
    {    
+        scoreBoard.IncreaseScore(enemyWorth);
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
    }
+   
 }
